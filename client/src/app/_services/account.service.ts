1ae223d236a462dfc15ecd4e6dc8db804aec6836
pukut <36571015+pukut@users.noreply.github.com>
2021-11-22 18:28:23 +0100

    End of section11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -31,8 +30,7 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         // we don't need this, but this would be the way to print out the user
         //return user;
@@ -41,6 +39,7 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
