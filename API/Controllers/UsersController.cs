dbbb5a058f4afef33585079a8db6e255465d38c8
pukut <36571015+pukut@users.noreply.github.com>
2021-12-06 14:45:24 +0100

    End of section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -55,7 +55,6 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        //[Authorize]
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
